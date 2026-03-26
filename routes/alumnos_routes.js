039b13494a071e716de2ddf2a2864d0099a50006
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-03-26 14:25:51 -0600

    Agregar consulta de alumno

--- routes/alumnos_routes.js
+++ routes/alumnos_routes.js
@@ -8,10 +8,14 @@ const {
     deleteAlumno
 } = require('../controllers/alumno_controller');
 
-router.get('/',         getAlumnos);
-router.get('/:id',      getAlumnoById);
-router.post('/',        createAlumno);
-router.put('/:id',      updateAlumno);
-router.delete('/:id',   deleteAlumno);
+router.get('/',              getAlumnos);
+router.get('/:id/detalle',   async (req, res) => {
+    const alumno = await require('../models/alumno').findById(req.params.id);
+    res.render('consultas', { alumno });
+});
+router.get('/:id',           getAlumnoById);
+router.post('/',             createAlumno);
+router.put('/:id',           updateAlumno);
+router.delete('/:id',        deleteAlumno);
 
 module.exports = router;
